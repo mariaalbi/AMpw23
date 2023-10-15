48aa7c724a9a39023a4b61de2ed855fbeeb298c1
Katarzyna Pszczolkowska <kasiaaa0802@gmail.com>
2023-10-15 10:12:20 +0200

    Added sweap_max to git

---
+++ exam.py
@@ -0,0 +1,17 @@
+def sweap_max(items: list) -> list:
+    max_item = items[0]
+    max_pos = 0
+    for i in range(len(items)):
+        if items[i] >= max_item:
+            max_item = items[i]
+            max_pos = i
+    # temp = items[0]
+    # items[0] = items[max_pos]
+    # items[max_pos] = temp
+    items[0], items[max_pos] = items[max_pos], items[0]
+    return items
+
+
+a = [5, 6, 2, 3, 4, 21, 13]
+result = sweap_max(a)
+print(result)
