8e807f746a6adef6c01b94198c2603df4f54d81c
Katarzyna Pszczolkowska <kasiaaa0802@gmail.com>
2023-10-15 11:25:10 +0200

    Add tuples example

--- exam.py
+++ exam.py
@@ -1,15 +1,15 @@
 def sweap_max(items: list) -> list:
     max_pos = items.index(max(items))
-    for i in range(len(items)):
-        if items[i] >= max_item:
-            max_item = items[i]
-            max_pos = i
+    # for i in range(len(items)):
+    #     if items[i] >= max_item:
+    #         max_item = items[i]
+    #         max_pos = i
     # temp = items[0]
     # items[0] = items[max_pos]
     # items[max_pos] = temp
     items[0], items[max_pos] = items[max_pos], items[0]
     return items
 
-a = [5, 6, 2, 3, 4, 21, 13]
+a = [5, 6, 2, 3, 42, 21, 13]
 result = sweap_max(a)
 print(result)
---
+++ tuples.py
@@ -0,0 +1,4 @@
+items = (1, 2, 3, 4, 5)
+
+for item in items:
+    print(item)
