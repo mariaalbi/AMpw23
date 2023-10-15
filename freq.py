675506c01db1c1587d81aa243570a2ae86be7dd4
Katarzyna Pszczolkowska <kasiaaa0802@gmail.com>
2023-10-15 16:01:00 +0200

    freq v2

--- freq.py
+++ freq.py
@@ -1,12 +1,15 @@
 from string import punctuation
+from collections import defaultdict
+
 def words_count(text: str) -> dict:
-    words = text.lower().replace(".", "").replace(",", "").split()
-    data = {}
-    for word in words:
-        if word not in data:
-            data[word] = 1
-        else:
-            data[word] += 1
+    lower_text = text.lower()
+    for ch in punctuation:
+        lower_text = lower_text.replace(ch, "")
+
+    data = defaultdict(int)
+    for word in lower_text.split():
+        data[word] += 1
     return data
-text = words_count("this Is is, is a. A a This sentence")
+
+text = words_count("this Is is, is a. A a! This sentence")
 print(text)
