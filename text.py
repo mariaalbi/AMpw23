166d5ce327b9106e9fd22ed619cd13ecfe6066ce
Katarzyna Pszczolkowska <kasiaaa0802@gmail.com>
2023-10-22 09:57:33 +0200

    text2

--- text.py
+++ text.py
@@ -1,11 +1,20 @@
-file = open("example.txt", "r")
-
-#lines = file.readlines()
-# for line in lines:
-#     #print(line.replace("\n",""))
+# file = open("example.txt", "r")
+#
+# #lines = file.readlines()
+# # for line in lines:
+# #     #print(line.replace("\n",""))
+# #     print(line, end = "")
+# # print("\n", lines)
+#
+# for line in file:
 #     print(line, end = "")
-# print("\n", lines)
+# file.close()
 
-for line in file:
-    print(line, end = "")
-file.close()
+def file_lines(filepath):
+    with open(filepath) as file:
+        for line in file:
+            yield line
+
+for line in file_lines("example.txt"):
+    print(line)
+
