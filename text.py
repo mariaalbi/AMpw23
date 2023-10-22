edef8650b303c17b182ddaf08368e905712e1c45
Katarzyna Pszczolkowska <kasiaaa0802@gmail.com>
2023-10-22 10:30:53 +0200

    text4

--- text.py
+++ text.py
@@ -26,10 +26,31 @@
 #
 # file.close()
 
-file = open("example.txt")
+# file = open("example.txt")
+#
+# result = file.read(10)
+# print(result)
+#
+# file.close()
 
-result = file.read(10)
-print(result)
+# with open("hello.txt", "w") as file:
+#     for _ in range(10):
+#         file.write("Hello world\n")
 
-file.close()
+# with open("hello.txt", "w") as file:
+#     file.writelines(["This is line 1\n",
+#                      "This is line 2\n"])
+
+# with open("hello.txt", "w") as file:
+#     file.writelines("This is line 1\n")
+
+with open("hello.txt", "a") as file:
+    for _ in range(10):
+        print("Hello", "world", sep=", ", file=file)
+
+# with open("hello.txt", "w") as file:
+#     print("This is line 1")
+
+
+
 
